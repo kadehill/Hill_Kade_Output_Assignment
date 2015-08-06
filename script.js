d8b628b0f48c81be2e309c7a546dde5382d4e784
kadehill <khill@fullsail.edu>
2015-08-05 22:47:50 -0400

    Output assignment version 4

--- script.js
+++ script.js
@@ -3,17 +3,17 @@
  */
 //Kade Hill SDI1508 Output Assignment
 
-var favColor;
-favColor = gold; //Assigning a String Variable
+var favColor = gold;
+    console.log(favColor); //Assigning a String Variable
 
-var isMarried;
-   isMarried = true; //Assigning a Boolean Variable
+var isMarried = true;
+    console.log(isMarried); //Assigning a Boolean Variable
 
-var isFemale;
-    isFemale = true; //Assigning a Boolean Variable
+var isFemale = true;
+    console.log(isFemale);  //Assigning a Boolean Variable
 
-var favFood;
-    favFood = Pasta; //Assigning a String Variable
+var favFood = Pasta;
+    console.log(favFood); //Assigning a String Variable
 
-var yearGraduates;
-    yearGraduates = 2017; //Assigning a Number Variable
+var yearGraduates = 2017;
+    console.log(yearGraduates); //Assigning a Number Variable
